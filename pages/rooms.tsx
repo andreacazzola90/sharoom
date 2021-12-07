bab5d53699d5119654ac92b2d335b186ae93d6ce
andrea cazzola <andreacazzola90@gmail.com>
2021-12-07 11:21:42 +0100

    add api for rooms

--- pages/rooms.tsx
+++ pages/rooms.tsx
@@ -3,92 +3,67 @@ import { Card, Image, Row, Col, Space, Typography, Button, Tag } from 'antd';
 import Layout from 'antd/lib/layout/layout';
 import Sider from 'antd/lib/layout/Sider';
 const { Title, Text, Link } = Typography;
-
+import { getAllPosts } from '../lib/api'
 import React, { useState } from 'react';
 
 
-export default function App() {
+
+export default function App({ allPosts }) {
+
+  const heroPost = allPosts[0]
+
   return (
     <div>
 
+      {JSON.stringify(allPosts)}
 
       <Layout className="site-layout-background" style={{}}>
         <Sider className="site-layout-background" width={400}></Sider>
 
         <Layout style={{ padding: '24px 48px' }}>
 
-          <Card style={{ padding: 12, marginBottom: 24 }}>
-            <Row>
-              <Col span={8}>
-                <Image height={300} src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
-              </Col>
-              <Col span={16} >
-                <Row justify="space-between">
-                  <Col>
-                    <Title level={2}>Stanza con letto matrimoniale</Title>
-                  </Col>
-                  <Col>
-                    <HeartOutlined style={{ fontSize: 25 }} />
-                  </Col>
-                </Row>
-                <Space size={5}><BorderOutlined /><Text>28 mq</Text></Space>
-                <Title level={4}>via indirizzo n.xx Città - (MI)</Title>
-                <Text>In appartamento abitato si affitta stanza con letto matrimoniale</Text>
-                <Row>
-                  <Col>
-                    <Space size={5}>
-                      <WifiOutlined /><FundProjectionScreenOutlined />
-                    </Space>
-                  </Col>
-                </Row>
-                <Row justify="space-between" align="bottom">
-                  <Col>
-                    <Tag color="blue">Privato</Tag>
-                  </Col>
-                  <Col>
-                    <Space direction="vertical" >
-                      <Text type="secondary">A partire da</Text>
-                      <Text >23€/notte</Text>
-                    </Space>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Card>
+          {allPosts.map((room: any) => {
+            return (<Card style={{ padding: 12, marginBottom: 24 }}>
+              <Row>
+                <Col span={8}>
+                  <Image height={300} src={room.coverImage} />
+                </Col>
+                <Col span={16} >
+                  <Row justify="space-between">
+                    <Col>
+                      <Title level={2}>{room.title}</Title>
+                    </Col>
+                    <Col>
+                      <HeartOutlined style={{ fontSize: 25 }} />
+                    </Col>
+                  </Row>
+                  <Space size={5}><BorderOutlined /><Text>28 mq</Text></Space>
+                  <Title level={4}>via indirizzo n.xx Città - (MI)</Title>
+                  <Text>In appartamento abitato si affitta stanza con letto matrimoniale</Text>
+                  <Row>
+                    <Col>
+                      <Space size={5}>
+                        <WifiOutlined /><FundProjectionScreenOutlined />
+                      </Space>
+                    </Col>
+                  </Row>
+                  <Row justify="space-between" align="bottom">
+                    <Col>
+                      <Tag color="blue">Privato</Tag>
+                    </Col>
+                    <Col>
+                      <Space direction="vertical" >
+                        <Text type="secondary">A partire da</Text>
+                        <Text >23€/notte</Text>
+                      </Space>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Card>)
+          })}
 
-          <Card style={{ padding: 12 }}>
-            <Row>
-              <Col span={8}>
-                <Image height={300} src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
-              </Col>
-              <Col span={16} >
-                <Title level={2}>Stanza con letto matrimoniale</Title>
-                <Space size={5}><BorderOutlined /><Text>28 mq</Text></Space>
-                <Title level={4}>via indirizzo n.xx Città - (MI)</Title>
-                <Text>In appartamento abitato si affitta stanza con letto matrimoniale</Text>
-                <Row>
-                  <Col>
-                    <Space size={5}>
-                      <WifiOutlined /><FundProjectionScreenOutlined />
-                    </Space>
-                  </Col>
-                </Row>
-                <Row justify="space-between" align="bottom">
-                  <Col>
-                    <Tag color="blue">Privato</Tag>
-                  </Col>
-                  <Col>
-                    <Space direction="vertical" >
-                      <Text type="secondary">A partire da</Text>
-                      <Text >23€/notte</Text>
-                    </Space>
-                  </Col>
-                </Row>
-              </Col>
-            </Row>
-          </Card>
         </Layout>
-
       </Layout>
 
 
@@ -96,7 +71,19 @@ export default function App() {
   )
 }
 
-export async function getServerSideProps({ req }) {
-  const headers = req ? req.headers : {};
-  return { props: { headers } }
+
+
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
 }
--- pages/rooms/room.tsx
+++ pages/rooms/room.tsx
@@ -2,21 +2,39 @@ import { BorderOutlined, FundProjectionScreenOutlined, HeartOutlined, WifiOutlin
 import { Card, Image, Row, Col, Space, Typography, Button, Tag, Layout } from 'antd';
 import Sider from 'antd/lib/layout/Sider';
 const { Title, Text, Link } = Typography;
+import { getAllPosts } from '../../lib/api'
 
 import React, { useState } from 'react';
 
+export async function getStaticProps() {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
 
-export default function App() {
+  return {
+    props: { allPosts },
+  }
+}
+
+export default function App({ allPosts }) {
   return (
+
     <div>
+
+      {JSON.stringify(allPosts[2])}
       <Layout className="site-layout-background" style={{ background: '#FFF' }}>
         <Row>
-          <Col span={12}>
+          <Col span={10}>
             <Image src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png" />
           </Col>
-          <Col span={12} >
+          <Col span={14} >
             <Layout style={{ padding: 24, background: '#FFF' }}>
-              <Title level={2}>Stanza con letto matrimoniale</Title>
+              <Title level={1}>Stanza con letto matrimoniale</Title>
               <Space size={5}><BorderOutlined /><Text>28 mq</Text></Space>
               <Title level={4}>via indirizzo n.xx Città - (MI)</Title>
               <Text>In appartamento abitato si affitta stanza con letto matrimoniale</Text>
@@ -27,17 +45,17 @@ export default function App() {
                   </Space>
                 </Col>
               </Row>
-              <Row justify="space-between" align="bottom">
+              <Row >
                 <Col>
                   <Tag color="blue">Privato</Tag>
                 </Col>
-                <Col>
-                  <Space direction="vertical" >
-                    <Text type="secondary">A partire da</Text>
-                    <Text >23€/notte</Text>
-                  </Space>
-                </Col>
               </Row>
+
+              <Space direction="vertical" >
+                <Text type="secondary">A partire da</Text>
+                <Text >23€/notte</Text>
+              </Space>
+
             </Layout>
           </Col>
         </Row>
@@ -49,7 +67,6 @@ export default function App() {
   )
 }
 
-export async function getServerSideProps({ req }) {
-  const headers = req ? req.headers : {};
-  return { props: { headers } }
-}
+
+
+
